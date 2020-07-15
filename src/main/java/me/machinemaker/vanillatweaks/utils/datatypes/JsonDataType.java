1eba1b1134b0712b4981946be7f28f38ec159c52
Professor Bloodstone <prof.bloodstone+git@gmail.com>
2020-07-15 22:12:13 +0200

    Add general PersistentDataType which uses GSON for arbitrary object serialization

---
+++ src/main/java/me/machinemaker/vanillatweaks/utils/datatypes/JsonDataType.java
@@ -0,0 +1,42 @@
+package me.machinemaker.vanillatweaks.utils.datatypes;
+
+import com.google.gson.Gson;
+import org.bukkit.persistence.PersistentDataAdapterContext;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Lets you store arbitrary data in PDC.
+ * @param <T>
+ */
+public class JsonDataType<T> implements PersistentDataType<String, T> {
+
+    private static final Gson gson = new Gson();
+    private final Class<T> typeClass;
+
+    public JsonDataType(Class<T> typeClass) {
+        this.typeClass = typeClass;
+    }
+
+    @Override
+    public @NotNull Class<String> getPrimitiveType() {
+        return String.class;
+    }
+
+    @Override
+    public @NotNull Class<T> getComplexType() {
+        return typeClass;
+    }
+
+    @NotNull
+    @Override
+    public String toPrimitive(@NotNull T complex, @NotNull PersistentDataAdapterContext persistentDataAdapterContext) {
+        return gson.toJson(complex);
+    }
+
+    @NotNull
+    @Override
+    public T fromPrimitive(@NotNull String primitive, @NotNull PersistentDataAdapterContext context) {
+        return gson.fromJson(primitive, getComplexType());
+    }
+}
