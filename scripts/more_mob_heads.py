61c80f5e8bad72d39f1bbccfcf2cf41dd0ce7414
Jake Potrebic <jake.m.potrebic@gmail.com>
2022-06-14 11:37:02 -0700

    feat: update MoreMobHeads for 1.19

--- scripts/more_mob_heads.py
+++ scripts/more_mob_heads.py
@@ -1,4 +1,4 @@
-from glob import iglob
+from glob import glob
 from json import load, dumps
 from os.path import abspath
 from sys import argv, exit
@@ -22,44 +22,53 @@ def run():
 
     missing = []
     heads = []
-    for filename in sorted(iglob(abspath(argv[1]) + '/**/*.json', recursive=True)):
-        done = False
-        with open(filename, "r") as file:
-            json = load(file)
-            loot_table_name = filename.split("/")[-1]
-            if "sheep" in filename:
-                loot_table_name = "/".join(filename.split("/")[-2:])
-            if "pools" in json:
-                for pool in json["pools"]:
-                    for entry in pool["entries"]:
-                        if "item" == entry["type"] and "minecraft:player_head" == entry["name"]:
-                            parse_head(entry, pool["conditions"], loot_table_name, heads)
-                            done = True
-                        elif "minecraft:alternatives" == entry["type"] and "children" in entry:
-                            for entry1 in entry["children"]:
-                                if "item" == entry1["type"] and "minecraft:player_head" == entry1["name"]:
-                                    conditions = entry["conditions"]
-                                    if "conditions" in entry1:
-                                        conditions += entry1["conditions"]
-                                    parse_head(entry1, conditions, loot_table_name, heads)
-                                    done = True
-
-        if not done and not (filename.endswith("shulker.json") or filename.endswith("ender_dragon.json")):
+    root = abspath(argv[1]) + '/entities'
+    files = sorted(glob(root + "/*.json") + glob(root + "/**[!reference]/*.json"))
+    for filename in files:
+        done = parse_table(root, heads, filename)
+        if not done:
             missing.append(filename)
 
     if len(missing):
         print("==== Missing ====")
         print("\n".join(sorted(missing)))
+        print("Address missing tables, and re-run script")
         return
 
     with open("../vanilla-tweaks-bukkit/src/main/resources/data/more_mob_heads.json", "w") as out:
-        out.write(dumps(heads, ensure_ascii=False, indent=2))
+        out.write(dumps(sorted(heads, key=lambda d: d["tableName"]), ensure_ascii=False, indent=2))
 
 
-    # print("\n".join([str(head["needsPlayer"]) for head in heads]))
+def parse_table(file_root: str, heads: list, filename: str) -> bool:
+    with open(filename, "r") as file:
+        done = False
+        json = load(file)
+        loot_table_name = filename.split("/")[-1]
+        if "sheep" in filename:
+            loot_table_name = "/".join(filename.split("/")[-2:])
+        if "ender_dragon" in filename:
+            return True
+        if "pools" in json:
+            for pool in json["pools"]:
+                for entry in pool["entries"]:
+                    if "item" == entry["type"] and "minecraft:player_head" == entry["name"]:
+                        parse_head(entry, pool["conditions"], loot_table_name, heads)
+                        done = True
+                    elif "alternatives" in entry["type"] and "children" in entry:
+                        for entry1 in entry["children"]:
+                            if "item" == entry1["type"] and "minecraft:player_head" == entry1["name"]:
+                                conditions = entry["conditions"]
+                                if "conditions" in entry1:
+                                    conditions += entry1["conditions"]
+                                parse_head(entry1, conditions, loot_table_name, heads)
+                                done = True
+                    elif "loot_table" in entry["type"] and "reference/mmh" in entry["name"]:
+                        if parse_table(file_root, heads, file_root + "/reference/mmh/" + loot_table_name):
+                            done = True
+    return done
 
 
-def parse_head(entry, conditions, table_name, heads):
+def parse_head(entry, conditions, table_name: str, heads: list):
     nbt = parse_nbt(entry["functions"][0]["tag"])
 
     needs_player = False
