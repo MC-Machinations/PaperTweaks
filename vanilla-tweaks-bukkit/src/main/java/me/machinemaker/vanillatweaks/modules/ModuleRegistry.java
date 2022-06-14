4410e097acca52460a7c6261af3327edc580bf6c
Jake Potrebic <jake.m.potrebic@gmail.com>
2022-06-14 14:53:02 -0700

    fix: fatal error if invalid modules.yml keys

--- vanilla-tweaks-bukkit/src/main/java/me/machinemaker/vanillatweaks/modules/ModuleRegistry.java
+++ vanilla-tweaks-bukkit/src/main/java/me/machinemaker/vanillatweaks/modules/ModuleRegistry.java
@@ -28,16 +28,19 @@ import io.github.classgraph.AnnotationInfo;
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfo;
 import io.github.classgraph.ScanResult;
-import me.machinemaker.lectern.ConfigurationNode;
-import me.machinemaker.lectern.YamlConfiguration;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import java.nio.file.Path;
 import java.util.Locale;
 import java.util.Map;
+import me.machinemaker.lectern.ConfigurationNode;
+import me.machinemaker.lectern.YamlConfiguration;
+import me.machinemaker.vanillatweaks.LoggerFactory;
+import me.machinemaker.vanillatweaks.config.LoggingInvalidKeyHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
 
 public class ModuleRegistry extends AbstractModule {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger();
     private static final int MODULE_FILE_VERSION = 1;
     private static final String MODULE_PKG = "me.machinemaker.vanillatweaks.modules";
     private static final String MODULE_INFO_ANNOTATION = "me.machinemaker.vanillatweaks.annotations.ModuleInfo";
@@ -46,27 +49,27 @@ public class ModuleRegistry extends AbstractModule {
     private final Map<String, Class<? extends ModuleBase>> modules = Maps.newHashMap();
 
     @SuppressWarnings("unchecked")
-    public ModuleRegistry(JavaPlugin plugin, Path dataPath) {
-        this.moduleConfig = YamlConfiguration.builder(dataPath.resolve("modules.yml")).build();
+    public ModuleRegistry(final JavaPlugin plugin, final Path dataPath) {
+        this.moduleConfig = YamlConfiguration.builder(dataPath.resolve("modules.yml")).withInvalidKeyHandler(new LoggingInvalidKeyHandler(LOGGER)).build();
         this.moduleConfig.set("version", MODULE_FILE_VERSION);
-        try (ScanResult scanResult = new ClassGraph().enableAnnotationInfo().acceptPackages(MODULE_PKG).scan()) {
-            for (ClassInfo classInfo : scanResult.getClassesWithAnnotation(MODULE_INFO_ANNOTATION)) {
-                AnnotationInfo annotationInfo = classInfo.getAnnotationInfo(MODULE_INFO_ANNOTATION);
-                String name = (String) annotationInfo.getParameterValues().getValue("name");
-                String configPath = (String) annotationInfo.getParameterValues().getValue("configPath");
-                modules.put(name.toLowerCase(Locale.US), (Class<? extends ModuleBase>) classInfo.loadClass());
-                moduleConfig.set(configPath, false);
+        try (final ScanResult scanResult = new ClassGraph().enableAnnotationInfo().acceptPackages(MODULE_PKG).scan()) {
+            for (final ClassInfo classInfo : scanResult.getClassesWithAnnotation(MODULE_INFO_ANNOTATION)) {
+                final AnnotationInfo annotationInfo = classInfo.getAnnotationInfo(MODULE_INFO_ANNOTATION);
+                final String name = (String) annotationInfo.getParameterValues().getValue("name");
+                final String configPath = (String) annotationInfo.getParameterValues().getValue("configPath");
+                this.modules.put(name.toLowerCase(Locale.US), (Class<? extends ModuleBase>) classInfo.loadClass());
+                this.moduleConfig.set(configPath, false);
             }
         }
-        moduleConfig.reloadAndSave();
+        this.moduleConfig.reloadAndSave();
     }
 
     @Override
     protected void configure() {
-        MapBinder<String, ModuleBase> moduleMapBinder = MapBinder.newMapBinder(binder(), String.class, ModuleBase.class);
-        modules.forEach((name, moduleClass) -> moduleMapBinder.addBinding(name).to(moduleClass).in(Scopes.SINGLETON));
-        modules.forEach((name, moduleClass) -> bind(moduleClass).in(Scopes.SINGLETON));
-        bind(ConfigurationNode.class).annotatedWith(Names.named("modules")).toInstance(moduleConfig);
-        bind(ModuleManager.class).in(Scopes.SINGLETON);
+        final MapBinder<String, ModuleBase> moduleMapBinder = MapBinder.newMapBinder(this.binder(), String.class, ModuleBase.class);
+        this.modules.forEach((name, moduleClass) -> moduleMapBinder.addBinding(name).to(moduleClass).in(Scopes.SINGLETON));
+        this.modules.forEach((name, moduleClass) -> this.bind(moduleClass).in(Scopes.SINGLETON));
+        this.bind(ConfigurationNode.class).annotatedWith(Names.named("modules")).toInstance(this.moduleConfig);
+        this.bind(ModuleManager.class).in(Scopes.SINGLETON);
     }
 }
